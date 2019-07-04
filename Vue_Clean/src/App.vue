b95d6a977e3ed31b6c4fe06c859edcbb5d92edc9
Joakim Flink <39080802+abbjoafli@users.noreply.github.com>
2019-07-04 13:24:54 +0200

    upload

---
+++ Vue_Clean/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Movies</span>
+        <span class="font-weight-light"> Super database</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    <v-btn
+        flat
+        target="_blank"
+        @click="addnew= !addnew"
+      >
+      
+        <span  v-if="addnew!==true" class="mr-2">Lägg till film</span>
+        <span  v-else class="mr-2">Kolla filmer</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+    <div v-if="addnew!==true">
+     
+    <ExampleComp MSG="apa apa apa" />
+      </div>
+          <div v-else>
+     
+      </div>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import ExampleComp from './components/ExampleComp'
+
+export default {
+  name: 'App',
+  components: {
+    ExampleComp
+  },
+  data () {
+    return {
+      addnew:false,
+      //
+    }
+  }
+}
+</script>
